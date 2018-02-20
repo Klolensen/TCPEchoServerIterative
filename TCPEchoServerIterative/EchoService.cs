808ffa8e2eeb479e700b2c0117285f00b157bb32
Klolensen <31515047+Klolensen@users.noreply.github.com>
2018-02-20 12:38:07 +0100

    Made it

---
+++ TCPEchoServerIterative/EchoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace TCPEchoServerIterative
+{
+    class EchoService
+    {
+        private TcpClient _tcp;
+
+        public EchoService(TcpClient tcp)
+        {
+            _tcp = tcp;
+        }
+
+        public void DoIt()
+        {
+            while (true)
+            {
+                Console.WriteLine("Ready for client");
+                Stream networkStream = _tcp.GetStream();
+                StreamReader streamReader = new StreamReader(networkStream);
+                StreamWriter streamWriter = new StreamWriter(networkStream);
+                streamWriter.AutoFlush = true;
+
+                string message = streamReader.ReadLine();
+                string answer = "";
+
+                while (message != null && message != "")
+                {
+                    Console.WriteLine("Client: " + message);
+                    answer = message.ToUpper();
+                    streamWriter.WriteLine(answer);
+                    message = streamReader.ReadLine();
+                }
+            }
+        }
+    }
+}
+
--- TCPEchoServerIterative/Program.cs
+++ TCPEchoServerIterative/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,17 @@ namespace TCPEchoServerIterative
     {
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Parse("10.200.123.145");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+            serverSocket.Start();
+            Console.WriteLine("Server started");
+            
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            EchoService service = new EchoService(connectionSocket);
+            service.DoIt();
+                
+
+            
         }
     }
 }
